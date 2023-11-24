645b80ef1e7d8efaf23d22237bf615f3cef51586
Nicos Nicolaou <nicosnicos16@gmail.com>
2023-11-24 22:54:17 +0200

    minor fix

--- lib/views/ships_screen/ships_bloc/ships_bloc.dart
+++ lib/views/ships_screen/ships_bloc/ships_bloc.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:samplefluttertvapp/data/database/entities/ships_entity.dart';
 import 'package:samplefluttertvapp/data/models/ships/ships_data_model.dart';
-import 'package:samplefluttertvapp/domain/repositories/ships_repository.dart';
+import 'package:samplefluttertvapp/data/repositories/ships_repository.dart';
 import 'package:samplefluttertvapp/views/ships_screen/ships_bloc/ships_events.dart';
 import 'package:samplefluttertvapp/views/ships_screen/ships_bloc/ships_states.dart';
 
