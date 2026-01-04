f782c08d65f2d827f94a13532e1998c295b34f1e
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-01-05 01:47:44 +0200

    update Gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     namespace = "com.samplefluttertvapp"
     compileSdk = 36
     buildToolsVersion = "36.0.0"
-    ndkVersion = "28.2.13676358"
+    ndkVersion = "29.0.14206865"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_21
